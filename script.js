587b0a3350b4a5af508a8b4339e6cf26060c2834
Ritaban Datta <34961676+Reetoo@users.noreply.github.com>
2021-09-29 08:38:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+<html>
+    <head>
+        <title>Simple Interest Calculator</title>
+<script>
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+  var  principal = document.getElementById("principal").value;
+ var rate=document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+ interest=document.getElementById("interest").value;
+ interest.innerText="The interest is " + (principal*years*rate/100);
+ var year=new Date().getFullYear()+parseInt(years);
+
 }
-        
+</script>
+</head>
+<body>
+    <h1>Simple Interest Calculator</h1>
+
+    Amount: <input id="principal"><br/>
+    Rate: <input id="rate"><br/>
+    No.of Years: <input  id="years"><br/>
+    <button onclick="compute()">compute interest</button>
+    <p id="interest"></p>
+
+    <script>
+        function updateRate(rateval){
+            var rateval=document.getElementById("rate").value;
+            document.getElementById("rate_val").innerText=rateval;
+        }
+    </script>
+
+</body>
+</html>
